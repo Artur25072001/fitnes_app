6557edcb9d37739fdd4f6eafd8b366795ef81ce4
Artur25072001 <arturdymchevskyi@gmail.com>
2026-02-11 21:03:36 +0000

    Deploying to gh-pages from @ Artur25072001/fitnes_app@1d820bf24e660b34fdda5c2edcc6bdbac41b94fa 🚀

--- favorites.js
+++ favorites.js
@@ -1,2 +1,2 @@
-import{i as o}from"./assets/modal-utils-DqoO7kMG.js";import"./assets/handler-favorites-BDTsTrgw.js";document.addEventListener("DOMContentLoaded",()=>{o(window.location.href)});
+import{i as o}from"./assets/modal-utils-BWLxLC2h.js";import"./assets/handler-favorites-BUezchmC.js";document.addEventListener("DOMContentLoaded",()=>{o(window.location.href)});
 //# sourceMappingURL=favorites.js.map
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import{i as o}from"./assets/modal-utils-DqoO7kMG.js";import"./assets/handler-main-DMgpbT0W.js";import"./assets/handler-modal-CBphlW6U.js";import"./assets/handlers-CXM7YiP2.js";document.addEventListener("DOMContentLoaded",()=>{o(window.location.href)});
+import{i as o}from"./assets/modal-utils-BWLxLC2h.js";import"./assets/handler-main-B-YzIoWg.js";import"./assets/handler-modal-CruYp-cv.js";import"./assets/handlers-CIc0sTk8.js";document.addEventListener("DOMContentLoaded",()=>{o(window.location.href)});
 //# sourceMappingURL=index.js.map
