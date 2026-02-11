05449b322c1a9cf83d145da3346f96d292b18891
Artur25072001 <arturdymchevskyi@gmail.com>
2026-02-11 19:17:50 +0000

    Deploying to gh-pages from @ Artur25072001/fitnes_app@d3da5167ed35dd215c7d8de9ba06d36ef95941a1 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import{i as o}from"./assets/modal-utils-BNKwfl0K.js";import"./assets/handler-main-BKJ6C9bm.js";import"./assets/handler-modal-DkI4qb-Y.js";import"./assets/handlers-hZ6HgKmN.js";document.addEventListener("DOMContentLoaded",()=>{o(window.location.href)});
+import{i as o}from"./assets/modal-utils-CBa532Ng.js";import"./assets/handler-main-DuhYvHle.js";import"./assets/handler-modal-DvAeXxIQ.js";import"./assets/handlers-BBbG2hyC.js";document.addEventListener("DOMContentLoaded",()=>{o(window.location.href)});
 //# sourceMappingURL=index.js.map
